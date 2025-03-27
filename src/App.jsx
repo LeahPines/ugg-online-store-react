aa150c3d7bfe1c2f0d0ce2ca126b432a860ea01d
LeahPines <leajp9973@gmail.com>
2025-03-27 23:43:05 +0200

    upload changes in docker details

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import AllProducts from './components/products/AllProducts'
 import About from './components/About'
 import Cart from './components/cart/Cart'
 import CartIcon from "./assets/add-button.png";
-import Logo from "./assets/ugg-logo.png";
+//import Logo from "./assets/ugg-logo.png";
 import HomePage from './components/HomePage'
 
 function App() {
@@ -15,7 +15,8 @@ function App() {
         <ul className="nav nav-underline">
           <li>
           <NavLink className="nav-link" to="/home" activeClassName="active">
-            <img src={Logo} alt="Logo" height="40px" />
+            {/* <img src={Logo} alt="Logo" height="40px" /> */}
+            <img src="https://storage.googleapis.com/docker-app-bucket/ugg-logo.png" alt="Logo" height="40px" />       
           </NavLink>
           </li>
           <li className="nav-item">
